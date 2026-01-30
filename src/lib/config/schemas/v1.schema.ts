2ae3d74f976fd1dd34521e7ab47feede543cd6c7
Muhammad Faizan <dev@mfaizan.com>
2026-01-30 23:48:12 +0530

    Remove use of extend in zod and use spread oprator.

--- src/lib/config/schemas/v1.schema.ts
+++ src/lib/config/schemas/v1.schema.ts
@@ -6,11 +6,14 @@ z.config({ jitless: true });
 
 // This change is small enough to use schema composition methods but
 // always create a new schema to prevent accidental mutations
-export const UserConfigSchemaV1 = UserConfigSchemaV0.omit({
-	migrationId: true,
-	customEngines: true
-}).extend({
-	schemaVersion: z.literal(1)
-});
+export const UserConfigSchemaV1 = z
+	.object({
+		...UserConfigSchemaV0.shape,
+		schemaVersion: z.literal(1)
+	})
+	.omit({
+		migrationId: true,
+		customEngines: true
+	});
 
 export type UserConfigTypeV1 = z.infer<typeof UserConfigSchemaV1>;
